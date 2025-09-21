8048fab119f619f40e1450faa2c3cadfa0e69b36
fhsuqwo <63940620+kty5788@users.noreply.github.com>
2025-09-22 01:50:05 +0900

    Initial Commit

---
+++ Linear-Algebra/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Linear-Algebra
+//
+//  Created by 강태윤 on 9/22/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
